822a4a91867d509c711517527022ae4622f1fdad
Konstantin Gerasimov <gerasimov.kostja@gmail.com>
2015-02-28 17:21:35 +0200

    Initial commit

--- Repeater/Repeater.swift
+++ Repeater/Repeater.swift
@@ -2,12 +2,86 @@
 //  Repeater.swift
 //  Repeater
 //
-//  Created by Konstantin Gerasimov on 2/28/15.
+//  Created by Konstantin Gerasimov on  7/9/14.
 //
 //
 
-import UIKit
+import Cocoa
 
-class Repeater: NSObject {
-   
+class Repeater {
+    
+    var repeatTimeoutMilliseconds: Int = 1000
+    var repeatAccuracyMilliseconds: Int = 1
+    var isRepeating = false
+    var timer: dispatch_source_t?
+    
+    deinit {
+        stop()
+    }
+    
+    
+    init() {
+    }
+    
+    convenience init(timeoutMs: Int, accuracyMs: Int) {
+        self.init()
+        repeatTimeoutMilliseconds = timeoutMs
+        repeatAccuracyMilliseconds = accuracyMs
+    }
+    
+    
+    func repeat(repeat: (()->())) {
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        repeatInQueue(queue, repeat: repeat)
+    }
+    
+    
+    func repeatInQueue(queue: dispatch_queue_t, repeat: (() -> ())) {
+        repeatInQueue(queue, timeoutMs: repeatTimeoutMilliseconds, accuracyMs: repeatAccuracyMilliseconds, repeat: repeat)
+    }
+    
+    func repeatWithTimeout(timeoutSec: Double, repeat: (() -> ())) {
+        repeatInQueue(dispatch_get_main_queue(), timeoutMs: Int(timeoutSec * 1000), accuracyMs: Int(timeoutSec), repeat: repeat)
+    }
+    
+    func repeatWithTimeout(timeoutSec: Int, repeat: (() -> ())) {
+        repeatInQueue(dispatch_get_main_queue(), timeoutMs: timeoutSec * 1000, accuracyMs: timeoutSec, repeat: repeat)
+    }
+    
+    func repeatInBgWithTimeout(timeoutSec: Double, repeat: (() -> ())) {
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        repeatInQueue(queue, timeoutMs: Int(timeoutSec * 1000), accuracyMs: Int(timeoutSec), repeat: repeat)
+    }
+    
+    func repeatInQueue(queue: dispatch_queue_t, var timeoutMs: Int, var accuracyMs: Int, repeat: (() -> ())) {
+        
+        stop()
+        isRepeating = true
+        
+        if timeoutMs < 1 {
+            timeoutMs = 1
+        }
+        if accuracyMs < 1 {
+            accuracyMs = 1
+        }
+        
+        let interval: UInt64 = NSEC_PER_MSEC * UInt64(timeoutMs)
+        let leeway: UInt64 = NSEC_PER_MSEC * UInt64(accuracyMs)
+        timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
+        if let existedTimer = timer {
+            dispatch_source_set_timer(existedTimer, dispatch_walltime(nil, 0), interval, leeway)
+            dispatch_source_set_event_handler(existedTimer) {
+                repeat()
+            }
+            dispatch_resume(existedTimer)
+        }
+    }
+    
+    func stop() {
+        if let existedTimer = timer {
+            dispatch_source_cancel(existedTimer);
+        }
+        isRepeating = false
+    }
 }
+
